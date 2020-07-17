a9a9fc02e8989422a25a4193271d3a26c23d4873
Yonipineda <yonipineda1010@icloud.com>
2020-07-16 23:32:00 -0400

    sprint done.

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -25,9 +25,77 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
+
+# Logic for sprint 
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def peek(self):
+        return self.stack[-1]
+    def size(self):
+        return len(self.stack)
+
+def traverse_maze(player):
+    # What we will return
+    traversal_path = []  # List of tuples
+    
+    opposite_direction = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
+
+    # rooms we have visited and rooms ahead
+    visited_rooms = set() 
+    s = Stack()
+
+    cur_room = player.current_room
+
+    # added two initially because I instantly pop one. Probably not good practice.
+    # s.push(cur_room) # current, room and direction traveled
+
+    while len(visited_rooms) != 500:
+        # Cur room id
+
+        if cur_room.id not in visited_rooms:
+            visited_rooms.add(cur_room.id)
+
+        prev_room = cur_room
+
+        # choose a direction
+        for direction in cur_room.get_exits():
+            if cur_room.get_room_in_direction(direction).id not in visited_rooms: 
+                # updates
+                s.push(opposite_direction[direction])
+                player.travel(direction)
+                traversal_path.append(direction)
+                visited_rooms.add(player.current_room.id)
+
+                cur_room = player.current_room
+                break # so we don't actually loop over the other exits. Just the first unexplored
+        
+        # This is what makes us traverse. Means it wasn't updated in loop above.
+        if cur_room == prev_room:
+            direction = s.pop()
+            player.travel(direction)
+            traversal_path.append(direction)
+
+            cur_room = player.current_room
+
+
+        # Choose a random direction to travse
+            # keep track of forks in the road, so when we get to a dead end we can traverse to an unexplored fork
+
+    return list(traversal_path)
+
+
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-traversal_path = []
+traversal_path = traverse_maze(player)
 
 
 
